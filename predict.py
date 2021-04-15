9dfa8b418860e232e123671ee0ec4b219baa9e52
Andreas Gaupmann <andreas@gaupmann.net>
2021-04-15 16:50:50 +0200

    Reformated comment to avoid linting error
    W0105: String statement has no effect (pointless-string-statement)

--- predict.py
+++ predict.py
@@ -15,6 +15,24 @@ def predict():
     """Performs an sklearn prediction
        result looks like:
     { "prediction": [ 20.35373177134412 ] }
+    
+    The Boston Housing Dataset
+    https://www.cs.toronto.edu/~delve/data/boston/bostonDetail.html
+    There are 14 attributes in each case of the dataset. They are:
+        CRIM - per capita crime rate by town
+        ZN - proportion of residential land zoned for lots over 25,000 sq.ft.
+        INDUS - proportion of non-retail business acres per town.
+        CHAS - Charles River dummy variable (1 if tract bounds river; 0 otherwise)
+        NOX - nitric oxides concentration (parts per 10 million)
+        RM - average number of rooms per dwelling
+        AGE - proportion of owner-occupied units built prior to 1940
+        DIS - weighted distances to five Boston employment centres
+        RAD - index of accessibility to radial highways
+        TAX - full-value property-tax rate per $10,000
+        PTRATIO - pupil-teacher ratio by town
+        B - 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+        LSTAT - % lower status of the population
+        MEDV - Median value of owner-occupied homes in $1000's
     """
 
     json_payload = {
@@ -55,23 +73,3 @@ def predict():
 
 if __name__ == "__main__":
     print(predict())
-
-"""
-The Boston Housing Dataset
-https://www.cs.toronto.edu/~delve/data/boston/bostonDetail.html
-There are 14 attributes in each case of the dataset. They are:
-    CRIM - per capita crime rate by town
-    ZN - proportion of residential land zoned for lots over 25,000 sq.ft.
-    INDUS - proportion of non-retail business acres per town.
-    CHAS - Charles River dummy variable (1 if tract bounds river; 0 otherwise)
-    NOX - nitric oxides concentration (parts per 10 million)
-    RM - average number of rooms per dwelling
-    AGE - proportion of owner-occupied units built prior to 1940
-    DIS - weighted distances to five Boston employment centres
-    RAD - index of accessibility to radial highways
-    TAX - full-value property-tax rate per $10,000
-    PTRATIO - pupil-teacher ratio by town
-    B - 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
-    LSTAT - % lower status of the population
-    MEDV - Median value of owner-occupied homes in $1000's
-"""
