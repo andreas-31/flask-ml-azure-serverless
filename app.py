923fa87b9230c1ce819bc844c2d2799ce62d951c
Andreas Gaupmann <andreas@gaupmann.net>
2021-04-21 10:40:25 +0000

    Welcome message changed

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home</h3><h4>Requests welcome!</h4>"
+    html = "<h3>Sklearn Prediction Home</h3><h4>Requests are welcome!</h4>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
